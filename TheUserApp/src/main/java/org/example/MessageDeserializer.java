5cc87f96a12d5c72b3b758dcc2116f67025ebc03
Samer-Ismael <samerismaeelbat2@hotmail.com>
2023-10-04 17:08:36 +0200

    Making custonm deserializer

--- TheUserApp/src/main/java/org/example/MessageDeserializer.java
+++ TheUserApp/src/main/java/org/example/MessageDeserializer.java
@@ -3,16 +3,20 @@ package org.example;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.kafka.common.serialization.Deserializer;
 
+
 public class MessageDeserializer implements Deserializer<Message> {
     private final ObjectMapper objectMapper = new ObjectMapper();
 
+    //method reads the binary data, attempts to parse it as JSON, and then
+    //converts it into a 'Message' object. If the deserialization fails for any reason, such as
+    //invalid data or a parsing error, it logs the exception and returns null.
     @Override
     public Message deserialize(String topic, byte[] data) {
         try {
             return objectMapper.readValue(data, Message.class);
         } catch (Exception e) {
             e.printStackTrace();
-            return null;
+            return null; // Return null to indicate deserialization failure
         }
     }
 
